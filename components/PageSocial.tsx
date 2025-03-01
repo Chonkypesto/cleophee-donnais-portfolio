b8fd07fb78cfda5101bd42b0547b68a6c62a378d
Chonkypesto <cleophee.sene@gmail.com>
2025-03-01 13:51:08 +0100

    InstagramShareButton config PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -23,26 +23,16 @@ const socialLinks: SocialLink[] = [
       </svg>
     )
   },
-
   config.instagram && {
-  name: 'instagram',
-  href: `https://instagram.com/${config.instagram}`,
-  title: `Instagram @${config.instagram}`,
-  icon: (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-      <path d="M7 2C3.7 2 2 3.7 2 7v10c0 3.3 1.7 5 5 5h10c3.3 0 5-1.7 5-5V7c0-3.3-1.7-5-5-5H7zm0 2h10c2 0 3 .8 3 3v10c0 2-.8 3-3 3H7c-2 0-3-.8-3-3V7c0-2 .8-3 3-3zm10 2a1 1 0 100 2 1 1 0 000-2zM12 6c-3.3 0-6 2.7-6 6s2.7 6 6 6 6-2.7 6-6-2.7-6-6-6zm0 2c2.2 0 4 1.8 4 4s-1.8 4-4 4-4-1.8-4-4 1.8-4 4-4z"></path>
-    </svg>
-  )
-},
-
-// config.github && {
-//   name: 'github',
-//   href: `https://github.com/${config.github}`,
-//   title: `GitHub @${config.github}`,
-//   icon: ( ... )
-// },
-
-
+    name: 'instagram',
+    href: `https://instagram.com/${config.instagram}`,
+    title: `Instagram @${config.instagram}`,
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M7 2C3.7 2 2 3.7 2 7v10c0 3.3 1.7 5 5 5h10c3.3 0 5-1.7 5-5V7c0-3.3-1.7-5-5-5H7zm0 2h10c2 0 3 .8 3 3v10c0 2-.8 3-3 3H7c-2 0-3-.8-3-3V7c0-2 .8-3 3-3zm10 2a1 1 0 100 2 1 1 0 000-2zM12 6c-3.3 0-6 2.7-6 6s2.7 6 6 6 6-2.7 6-6-2.7-6-6-6zm0 2c2.2 0 4 1.8 4 4s-1.8 4-4 4-4-1.8-4-4 1.8-4 4-4z"></path>
+      </svg>
+    )
+  },
   config.linkedin && {
     name: 'linkedin',
     href: `https://www.linkedin.com/in/${config.linkedin}`,
@@ -52,30 +42,8 @@ const socialLinks: SocialLink[] = [
         <path d='M6.5 21.5h-5v-13h5v13zM4 6.5C2.5 6.5 1.5 5.3 1.5 4s1-2.4 2.5-2.4c1.6 0 2.5 1 2.6 2.5 0 1.4-1 2.5-2.6 2.5zm11.5 6c-1 0-2 1-2 2v7h-5v-13h5V10s1.6-1.5 4-1.5c3 0 5 2.2 5 6.3v6.7h-5v-7c0-1-1-2-2-2z' />
       </svg>
     )
-  },
-
-  config.newsletter && {
-    name: 'newsletter',
-    href: `${config.newsletter}`,
-    title: `Newsletter ${config.author}`,
-    icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-        <path d='M12 .64L8.23 3H5V5L2.97 6.29C2.39 6.64 2 7.27 2 8V18C2 19.11 2.9 20 4 20H20C21.11 20 22 19.11 22 18V8C22 7.27 21.61 6.64 21.03 6.29L19 5V3H15.77M7 5H17V9.88L12 13L7 9.88M8 6V7.5H16V6M5 7.38V8.63L4 8M19 7.38L20 8L19 8.63M8 8.5V10H16V8.5Z' />
-      </svg>
-    )
-  },
-
-  config.youtube && {
-    name: 'youtube',
-    href: `https://www.youtube.com/${config.youtube}`,
-    title: `YouTube ${config.youtube}`,
-    icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-        <path d='M10,15L15.19,12L10,9V15M21.56,7.17C21.69,7.64 21.78,8.27 21.84,9.07C21.91,9.87 21.94,10.56 21.94,11.16L22,12C22,14.19 21.84,15.8 21.56,16.83C21.31,17.73 20.73,18.31 19.83,18.56C19.36,18.69 18.5,18.78 17.18,18.84C15.88,18.91 14.69,18.94 13.59,18.94L12,19C7.81,19 5.2,18.84 4.17,18.56C3.27,18.31 2.69,17.73 2.44,16.83C2.31,16.36 2.22,15.73 2.16,14.93C2.09,14.13 2.06,13.44 2.06,12.84L2,12C2,9.81 2.16,8.2 2.44,7.17C2.69,6.27 3.27,5.69 4.17,5.44C4.64,5.31 5.5,5.22 6.82,5.16C8.12,5.09 9.31,5.06 10.41,5.06L12,5C16.19,5 18.8,5.16 19.83,5.44C20.73,5.69 21.31,6.27 21.56,7.17Z' />
-      </svg>
-    )
   }
-].filter(Boolean)
+].filter(Boolean); // Ensures we remove any undefined values
 
 export function PageSocial() {
   return (
@@ -97,5 +65,6 @@ export function PageSocial() {
         </a>
       ))}
     </div>
-  )
+  );
 }
+
